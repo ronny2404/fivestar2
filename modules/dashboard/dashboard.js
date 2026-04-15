5f48aa6164d4b6bb85c7c4becbcfa369aae83690
ronny2404 <asroni.firdos24@gmail.com>
2026-04-16 02:34:43 +0700

    Delete dashboard.js

--- modules/dashboard/dashboard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('user-target').textContent = sessionStorage.getItem('session_user');
-
-document.getElementById('logout').onclick = () => {
-    sessionStorage.clear(); // Hapus Session
-    window.location.href = '../login/login.html';
-};
