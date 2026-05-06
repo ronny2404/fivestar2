4eeafa33e7806bebe1ade601a74ff9c3b09a9bb9
azkazio <asroni.firdos24@gmail.com>
2026-05-06 23:29:54 +0700

    Update main.js

--- js/core/main.js
+++ js/core/main.js
@@ -2,6 +2,11 @@
 // main.js - FIVE STAR 2 PREMIUM CORE (ULTIMATE STACKED NAVIGATION)
 // =============================================================================
 
+// Fungsi cek apakah user menggunakan iPhone/iPad
+const isIos = () => {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+};
+
 window.avatarSiluet = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23ffffff'%3E%3Cpath d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z'%3E%3C/path%3E%3C/svg%3E";
 
 function muatFotoDashboard() {
