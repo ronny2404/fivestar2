586b6adf457eab7d8baa9081ccba86acaaa53dce
azkazio <asroni.firdos24@gmail.com>
2026-05-08 02:37:00 +0700

    Update absen.js

--- js/features/absen.js
+++ js/features/absen.js
@@ -217,7 +217,7 @@ async function simpanAbsen(isPolisiMode = false) {
             }
         } else {
             // --- MODE TOAST (NORMAL) ---
-            IOSAlert.show("Berhasil", "Absen berhasil disimpan!"); history.go(-2);
+            IOSAlert.show("Berhasil", "Absen berhasil disimpan!"); history.go(-1);
             tutupMenuAbsen();
         }
     } catch (e) {
